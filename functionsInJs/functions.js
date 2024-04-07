aaa621d6f4e117f888899fcc024a163ca947eda0
snehitshh <snehitsh07@gmail.com>
2024-04-07 20:59:49 +0530

    additions

--- functionsInJs/functions.js
+++ functionsInJs/functions.js
@@ -17,7 +17,7 @@ function countVowels(str){
     }
     return count;
 }
-let arr=[1,2,3,4,5];              //for each funtion
+let arr=[1,2,3,4,5]; //for each funtion  , it is a higher order function(either takes another function as parameter or returns another func)
 arr.forEach(function printVal(value){
     console.log(value);
 })
@@ -37,3 +37,27 @@ let arr4=["delhi","pune","mumbai"];           //in arrow function format
 arr4.forEach((val)=> {         //for each is used if we want to change something in every value of array
 console.log(val.toUpperCase());
 });
+ 
+let nums=[1,2,3,4,5];             //map 
+let newArr=nums.map((val)=>{ //takes the values from another array and modifies it according to the use and places the them in a new array
+    return val*2;
+});
+
+let ar=[1,2,3,4,5,6];          //filter
+let evenArr=ar.filter((val)=>{ //filters out even values here i.e it works as a filter according to the operation and places in a new array
+    return val%2==0; 
+});
+console.log(evenArr);
+
+
+let arr5=[1,2,3,4,5];          //reduce 
+const output =arr.reduce((prev,curr)=>{
+    return prev+curr;
+})
+console.log(output);    
+
+let arr6=[1,2,3,4,5];
+const output1 =arr.reduce((prev,curr)=>{
+    return prev>curr?prev:curr;
+})
+console.log(output1);    
