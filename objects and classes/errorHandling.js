f6e5c1774dd09d62edd93af1fdf1efb557c47aaf
snehitshh <snehitsh07@gmail.com>
2024-04-18 14:39:17 +0530

    try catch

---
+++ objects and classes/errorHandling.js
@@ -0,0 +1,14 @@
+let a=5;
+let b=10;
+console.log(a);
+console.log(b);
+console.log(a+b);
+try{
+    console.log(a+c);  //error
+}
+catch(err){                //handle errors
+console.log(err);
+}
+
+console.log(a-b);  //these lines will not be printed
+console.log(a*b);
